b4cc0dba8c3622434b8452bd1cd5825a91255a89
syl9798@163.com <syl9798@163.com>
2019-07-28 22:26:05 +0800

    spring-boot+spring-data+h2

---
+++ src/main/java/com/example/h2JPA/H2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2JpaApplication.class, args);
+	}
+
+}
